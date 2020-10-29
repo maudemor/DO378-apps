2b26ab471ab0c0b2baff08a0a1452bbe083fe545
Manuel Aude Morales <maudemor@redhat.com>
2020-10-29 03:28:30 -0600

    Running tests for lab.

--- quarkus-conference/microservice-speaker/src/test/java/org/acme/conference/speaker/NativeSpeakerResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.conference.speaker;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeSpeakerResourceIT extends SpeakerResourceTest {
-
-    // Execute the same tests but in native mode.
-}
